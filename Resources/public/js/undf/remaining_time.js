f7cd1b954945707d108b77ed31c66f3bcb54fe0c
Hermes <hermes.carretero@undefined.es>
2013-05-29 11:28:23 +0200

    Add skinning for the u-remaining-time directive

--- Resources/public/js/undf/remaining_time.js
+++ Resources/public/js/undf/remaining_time.js
@@ -17,19 +17,42 @@ angular.module('uRemainingTime', []).directive('uRemainingTime', function($timeo
             // Calculate how many days the interval contains. Subtract that
             // many days from the interval to determine the remainder.
             var days = Math.floor(interval / msecPerDay);
+            var days1 = Math.floor(days / 10);
+            var days2 = days % 10;
             interval = interval - (days * msecPerDay);
 
             // Calculate the hours, minutes, and seconds.
             var hours = Math.floor(interval / msecPerHour);
+            var hours1 = Math.floor(hours / 10);
+            var hours2 = hours % 10;
             interval = interval - (hours * msecPerHour);
 
             var minutes = Math.floor(interval / msecPerMinute);
+            var minutes1 = Math.floor(minutes / 10);
+            var minutes2 = minutes % 10;
             interval = interval - (minutes * msecPerMinute);
 
             var seconds = Math.floor(interval / 1000);
+            var seconds1 = Math.floor(seconds / 10);
+            var seconds2 = seconds % 10;
 
             // Display the result.
-            element.text((days > 0 ? days + 'd ' : '') + hours + "h:" + minutes + "m:" + seconds + "s");
+            element.html('<ul class="mm-days">' + 
+                    '<li class=' + (days ? "mm-active" : "") +'>' + days1 + '</li>' +
+                    '<li class=' + (days ? "mm-active" : "") +'>' + days2 + '</li>' + 
+                '</ul>' +
+                '<ul class="mm-hours">' + 
+                    '<li class=' + (days || hours ? "mm-active" : "") +'>' + hours1 + '</li>' +
+                    '<li class=' + (days || hours ? "mm-active" : "") +'>' + hours2 + '</li>' + 
+                '</ul>' +
+                '<ul class="mm-minutes">' + 
+                    '<li class=' + (days || hours || minutes ? "mm-active" : "") +'>' + minutes1 + '</li>' +
+                    '<li class=' + (days || hours || minutes ? "mm-active" : "") +'>' + minutes2 + '</li>' + 
+                '</ul>' +
+                '<ul class="mm-seconds">' + 
+                    '<li class=' + (days || hours || minutes || seconds ? "mm-active" : "") +'>' + seconds1 + '</li>' +
+                    '<li class=' + (days || hours || minutes || seconds ? "mm-active" : "") +'>' + seconds2 + '</li>' + 
+                '</ul>')
         }
 
         // watch the expression, and update the UI on change.
